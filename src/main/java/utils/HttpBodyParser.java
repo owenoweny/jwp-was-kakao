55d4f88f9f968c7ba50712352adb0c1ca77bbede
owenoweny <na_chi@naver.com>
2024-04-11 14:25:34 +0900

    chore: 주석 삭제

--- src/main/java/utils/HttpBodyParser.java
+++ src/main/java/utils/HttpBodyParser.java
@@ -13,7 +13,6 @@ public class HttpBodyParser {
         if ("application/x-www-form-urlencoded".equals(contentType)) {
             return new HttpBodyParser(new FormUrlEncodedParsingStrategy());
         }
-        //예외를 얘가 던지는게 맞나?... HttpRequest가 던져야하지 않나?
         throw new IllegalArgumentException("파싱을 지원되지 않는 컨텐츠타입입니다.");
     }
 
